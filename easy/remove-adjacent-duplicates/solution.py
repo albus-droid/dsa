b40fd0f3338a0b34f10e7fce76b7f591c6788bf5
Albin Babu Varghese <albinbabuvarghese20@gmail.com>
2026-01-10 21:21:18 -0500

    Add test cases for q2

--- easy/remove-adjacent-duplicates/solution.py
+++ easy/remove-adjacent-duplicates/solution.py
@@ -9,3 +9,17 @@ class Solution:
             else:
                 stack.append(c) # or push
         return "".join(stack) # return stack elements joined
+
+# Testing the algorithm with example inputs
+def assert_test(got, expected):
+	print("Result :", got, " Expected: ", expected)
+	assert got == expected
+	assert type(got) is type(expected)
+
+if __name__ == "__main__":
+    solution = Solution()
+    assert_test(solution.removeDuplicates("abccba"), "") # Output: ""
+    assert_test(solution.removeDuplicates("foobar"), "fbar") # Output: "fbar"
+    assert_test(solution.removeDuplicates("abcd"), "abcd") # Output: "abcd"
+
+    print("\033[92m[OK]\033[0m All test cases passed")
