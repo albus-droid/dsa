428359d5fcd0b107b853f3bfaa310ab6c9804855
ALBIN BABU VARGHESE <albinbabuvarghese20@gmail.com>
2026-01-11 01:58:07 +0000

    Add test cases for isToeplitzMatrix function
    Added test cases to validate the Toeplitz matrix algorithm.

--- easy/toeplitz-matrix/solution.py
+++ easy/toeplitz-matrix/solution.py
@@ -6,3 +6,10 @@ class Solution:
                 if matrix[i][j] != matrix[i + 1][j + 1]:
                     return False
         return True
+
+# Testing the algorithm with example inputs
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isToeplitzMatrix([[1, 2, 3], [4, 1, 2], [5, 4, 1]]))  # true
+    print(solution.isToeplitzMatrix([[1, 2], [3, 4]]))  # false
+    print(solution.isToeplitzMatrix([[7, 7, 7], [7, 7, 7], [7, 7, 7]]))  # true
